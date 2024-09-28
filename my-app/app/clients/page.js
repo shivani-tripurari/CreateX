4a2c197cb3f8176ec14dbde276828f49d07072f6
Shivani <shivanitripurari07@gmail.com>
2024-09-28 15:11:36 +0530

    clients

--- my-app/app/clients/page.js
+++ my-app/app/clients/page.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import { clientInfo } from '../data';
+import Image from 'next/image';
+import Footer from '../components/Footer.jsx';
 
 const page = () => {
   return (
-    <div>
-      
+    <>
+    <div className='h-[300vh] md:h-[150vh] fold:h-[200vh] ipad-air:h-[100vh] xl:h-[120vh] bg-[#F5F5DC]'>
+        <div className='pt-10 p-7 flex flex-col justify-center items-center'>
+            <h1 className='pt-10 text-black font-bold text-5xl p-3'>Trusted by Brands, Big and Small</h1>
+            <h3 className='text-black font-regular text-xl p-3'>from Concept to Creation. Explore the magic behind every print we've brought to life.</h3>
+        </div>
+        <div className='p-7 flex justify-center items-center'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 ipad-air:grid-cols-2 xl:grid-cols-4 gap-4'>
+                {clientInfo.map((item) => (
+                    <div key={item.id} className='h-64 w-64 bg-white flex flex-col justify-center items-center'>
+                        <Image src={item.imageUrl} className='h-12 w-30 object-contain' />
+                        <h3 className='p-3 text-center text-black font-bold text-base md:text-lg xl:text-xl'>{item.title}</h3>
+                        <p className='p-3 text-center text-black/70 font-regular text-base '>{item.desc}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+
     </div>
+    <Footer/>
+    </>
   )
 }
 
--- my-app/app/data/index.js
+++ my-app/app/data/index.js
@@ -1,4 +1,7 @@
 import img1 from '../assets/img1.svg';
+import logo1 from '../assets/anurag.svg';
+import logo2 from '../assets/tedlogo.svg';
+import logo3 from '../assets/vlogo.svg';
 
 export const projectInfo = [
     {
@@ -50,4 +53,55 @@ export const projectInfo = [
         imgUrl: img1
     }
 
+]
+
+export const clientInfo = [
+    {
+        id:"0",
+        imageUrl: logo1,
+        title: "Anurag University",
+        desc:"Designed tshhirts, merch, posters"
+    },
+    {
+        id:"1",
+        imageUrl: logo2,
+        title: "TedX Hyderabad",
+        desc:"Designed tshhirts, merch, posters"
+    },
+    {
+        id:"2",
+        imageUrl: logo3,
+        title: "Farmers Table",
+        desc:"Designed tshhirts, merch, posters"
+    },
+    {
+        id:"3",
+        imageUrl: logo2,
+        title: "TedX Hyderabad",
+        desc:"Designed tshhirts, merch, posters"
+    },
+    {
+        id:"4",
+        imageUrl: logo3,
+        title: "Farmers Table",
+        desc:"Designed tshhirts, merch, posters"
+    },
+    {
+        id:"5",
+        imageUrl: logo1,
+        title: "Anurag University",
+        desc:"Designed tshhirts, merch, posters"
+    },
+    {
+        id:"6",
+        imageUrl: logo3,
+        title: "Framers Table",
+        desc:"Designed tshhirts, merch, posters"
+    },
+    {
+        id:"7",
+        imageUrl: logo2,
+        title: "TedX Hyderabad",
+        desc:"Designed tshhirts, merch, posters"
+    }
 ]
