fee034189438f888aeb64cfed9c429a31180d4d5
Shivani <shivanitripurari07@gmail.com>
2024-09-28 13:46:49 +0530

    upload

---
+++ my-app/app/projects/page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Image from 'next/image'
+import Projects from '../components/Projects.jsx'
+import Footer from '../components/Footer.jsx'
+
+const page = () => {
+  return (
+    <>
+        <Projects/>
+        <Footer/>
+    </>
+  )
+}
+
+export default page
---
+++ my-app/app/services/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Image from 'next/image'
+import Services from '../components/Services.jsx'
+import Footer from '../components/Footer.jsx'
+const page = () => {
+  return (
+    <>
+        <Services/>
+        <Footer/>
+    </>
+  )
+}
+
+export default page
